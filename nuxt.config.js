5ac7b3d83780f059180e21b187fa37669f6f2c26
Diego Dias Pereira <diegodiasptc@gmail.com>
2020-01-07 14:47:00 -0300

    Finalizando para deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     host: '0.0.0.0'
   },
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Diego Dias',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
